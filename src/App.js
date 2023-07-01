3be33aa3b3b9c89d9cda7786871df3aade43ca8c
natewhoms <natewhoms@gmail.com>
2023-07-01 14:48:02 -0400

    added particle background, banner img, and text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Particle from './components/Particle';
 
 function App() {
   return (
     <div className="App">
+      <Particle />
       <NavBar />
+      <Banner />
+      
     </div>
   );
 }

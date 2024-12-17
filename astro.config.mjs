f9a8ec8f5f87f1cb84400a3836f128882c219494
natewhoms <natewhoms@gmail.com>
2024-12-17 16:13:43 -0500

    deployed to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({
+      applyBaseStyles: true,
+    }),
+  ],
   site: 'https://natewhoms.github.io',
   base: 'natewhoms-github-io',
 });

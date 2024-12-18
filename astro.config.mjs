0cbc51dbedae6a74b3f83d2008248b063473e3c5
natewhoms <natewhoms@gmail.com>
2024-12-18 02:13:26 -0500

    Refactor Astro configuration: removed Node adapter and TypeScript support; updated dependencies in package.json and package-lock.json; deleted tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
-import node from '@astrojs/node';
-
 export default defineConfig({
-  integrations: [tailwind()
-  ],
-
-  site: 'https://natewhoms.github.io',
-  base: 'natewhoms-github-io',
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  integrations: [tailwind()],
 });

2f15e7376d8fd4cc2a4f34275a13602509b647d3
natewhoms <natewhoms@gmail.com>
2024-12-18 01:56:31 -0500

    Update Astro configuration and dependencies; added Node adapter and TypeScript support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
-  integrations: [tailwind({
-      applyBaseStyles: true,
-    }),
+  integrations: [tailwind()
   ],
+
   site: 'https://natewhoms.github.io',
   base: 'natewhoms-github-io',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
